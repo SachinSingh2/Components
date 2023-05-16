975132e2fc82fe49aa05c3e9ab30917010275fd7
Sachin <ss0985211@gmail.com>
2023-05-16 12:37:42 +0530

    We have completed our button component moving forward

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from './components/Button'
 
 function App() {
+
+  const MyFun = ()=>{
+    console.log("Hello world")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+ <div> <Button onClick={MyFun} primary rounded className='my-2 mx-2' >Primary</Button> </div>
+
+ <div> <Button onMouseOver={MyFun} success className='my-2 mx-2' >Secondary</Button> </div>
+
+ <div> <Button outline warning rounded className='my-2 mx-2'>Warning</Button> </div>
+
+ <div> <Button danger outline rounded className='my-2 mx-2'>Success</Button> </div>
+ 
+ <div> <Button secondary outline rounded className='my-2 mx-2'>Danger</Button> </div>
+
+    </>
   );
 }
+
 
 export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,65 @@
+import classnames from "classnames";
+
+function Button({
+  children,
+  primary,
+  secondary,
+  warning,
+  danger,
+  success,
+  outline,
+  rounded,
+  ...rest
+}) {
+  const classes = classnames(
+    rest.className,
+    "flex items-center px-3  py-1.5 border",
+    {
+
+    "border-blue-500 bg-blue-500": primary,
+    "border-gray-900 bg-gray-900": secondary,
+    "border-green-500 bg-green-500": success,
+    "border-yellow-400 bg-yellow-400": warning,
+    "border-red-500 bg-red-500": danger,
+    "rounded-full": rounded,
+    "text-white":
+      !outline && (primary || secondary || success || warning || danger),
+    "bg-inherit": outline,
+    "text-blue-500": outline && primary,
+    "text-gray-500": outline && secondary,
+    "text-green-500": outline && success,
+    "text-yellow-500": outline && warning,
+    "text-red-500": outline && danger,
+  }
+  );
+
+  return (
+    <>
+      <button {...rest} className={classes}>{children}</button>
+    </>
+  );
+}
+
+Button.propTypes = {
+    CheckVariationValue: ({
+    primary,
+    secondary,
+    warning,
+    danger,
+    success,
+  }) => {
+    let count =
+      Number(!!primary) +
+      Number(!!secondary) +
+      Number(!!warning) +
+      Number(!!success) +
+      Number(!!danger);
+    console.log(count);
+
+    if (count > 1) {
+      return new Error ("You can only use only one style")
+    }
+  },
+};
+
+export default Button;
