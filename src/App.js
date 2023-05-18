e338a1c0d8ad28b09e2d17349f3545d42c302163
Sachin <ss0985211@gmail.com>
2023-05-18 20:22:49 +0530

    Did  a little change created a new page and put all the app data in the dropdownpage

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-import { useState } from "react";
-import DropDown from "./components/DropDown";
+import React from 'react'
 
-function App() {
-  const [value , setValue] = useState(null)
 
-  const onChange = (option)=>{
-    setValue(option)
-  }
-
-  // Here we have created an array of object in which we have defined the data which we are going to show in the dropdown.
-  const DropDownData = [
-    {label:'Red' , value:'Red'},
-    {label:'Blue' , value:'Blue'},
-    {label:'Green' , value:'Green'},
-    {label:'Black' , value:'Black'},
-    {label:'Pink' , value:'Pink'}
-  ]
-
+export default function App() {
   return (
-    <>
-<DropDown value={value} onChange={onChange} DropDownData={DropDownData}/>
-    </>
-  );
+    <div>
+
+    </div>
+  )
 }
-
-
-export default App;
---
+++ src/pages/DropDownPage.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import DropDown from "../components/DropDown";
+
+function DropDownPage() {
+  const [value , setValue] = useState(null)
+
+  const onChange = (option)=>{
+    setValue(option)
+  }
+
+  // Here we have created an array of object in which we have defined the data which we are going to show in the dropdown.
+  const DropDownData = [
+    {label:'Red' , value:'Red'},
+    {label:'Blue' , value:'Blue'},
+    {label:'Green' , value:'Green'},
+    {label:'Black' , value:'Black'},
+    {label:'Pink' , value:'Pink'}
+  ]
+
+  return (
+    <>
+<DropDown value={value} onChange={onChange} DropDownData={DropDownData}/>
+    </>
+  );
+}
+
+
+export default DropDownPage;
